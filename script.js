48e6d945b0af700e328b3906df985c66118b921c
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2025-09-16 20:01:37 +0100

    Refactor the code and add smooth scrolling animation

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// Navbar Scroll Interactivity
+// Navbar Interactivity ‼️‼️
+
+// Scroll Interactivity
 const navContainer = document.querySelector('.primary-nav');
 const navLinks = document.querySelectorAll('.primary-nav a');
 
@@ -20,7 +22,7 @@ window.addEventListener('scroll', () => {
   }
 });
 
-// Navbar Click Interactivity
+// Menu Click Interactivity
 const bars = document.querySelector('.bars');
 const menu = document.querySelector('#menu');
 const closeBtn = document.querySelector('.close-btn i');
@@ -34,7 +36,49 @@ bars.addEventListener('click', () => {
 function closeMenu() {
   menu.classList.remove('nav-active');
   overlay.classList.add('hidden');
-};
+}
 
 closeBtn.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
+
+// Smooth Scrolling ‼️‼️
+
+// Buttons
+const homeSecBtn = document.querySelector('.home-sec-btn');
+const featuresSecBtn = document.querySelector('.features-sec-btn');
+const screenshotsSecBtn = document.querySelector('.screenshots-sec-btn');
+const pricingSecBtn = document.querySelector('.pricing-sec-btn');
+const teamSecBtn = document.querySelector('.team-sec-btn');
+const contactSecBtn = document.querySelector('.contact-sec-btn');
+
+// Sections (to scroll to)
+const homeSec = document.querySelector('#home-section');
+const featuresSec = document.querySelector('#features-section');
+const screenshotsSec = document.querySelector('#screenshots-section');
+const pricingSec = document.querySelector('#pricing-section');
+const teamSec = document.querySelector('#team-section');
+const contactSec = document.querySelector('#contact-section');
+
+homeSecBtn.addEventListener('click', () =>
+  homeSec.scrollIntoView({ behavior: 'smooth' })
+);
+
+featuresSecBtn.addEventListener('click', () =>
+  featuresSec.scrollIntoView({ behavior: 'smooth' })
+);
+
+screenshotsSecBtn.addEventListener('click', () =>
+  screenshotsSec.scrollIntoView({ behavior: 'smooth' })
+);
+
+pricingSecBtn.addEventListener('click', () =>
+  pricingSec.scrollIntoView({ behavior: 'smooth' })
+);
+
+teamSecBtn.addEventListener('click', () =>
+  teamSec.scrollIntoView({ behavior: 'smooth' })
+);
+
+contactSecBtn.addEventListener('click', () =>
+  contactSec.scrollIntoView({ behavior: 'smooth' })
+);
